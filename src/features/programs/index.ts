d49a452ddc867038f9e257d74a29237d1235a1ff
2meawmeaw2 <tahaslco@gmail.com>
2026-03-03 14:25:30 +0100

    Add typed weekly program plan builder

---
+++ src/features/programs/index.ts
@@ -0,0 +1,13 @@
+export { buildProgram } from './planBuilder';
+export type {
+  ExerciseBlock,
+  IntensityMetric,
+  MovementCategory,
+  Program,
+  ProgramBuilderInput,
+  ProgressionMethod,
+  ProgressionRule,
+  Session,
+  SkillTrack,
+  TrainingWeek,
+} from './models';
